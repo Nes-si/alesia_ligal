7a4a45172a5a2c0ce257341d35c708442e11fe5a
Никита Головин <golovin.nik@gmail.com>
2015-12-22 23:10:32 +0300

    Страница 0.3.4

--- javascripts/app.js
+++ javascripts/app.js
@@ -111,7 +111,7 @@
 })();
 require.register("initialize", function(exports, require, module) {
 $(function() {
-  var back_color, caption, caption1_w, caption1_width, caption2_w, caption2_width, caption3_w, caption3_width, caption4_w, caption4_width, car_anim, controller, scene;
+  var back_color, caption, car_anim, controller, scene;
   $('.slider').slick({
     dots: false,
     arrows: false,
@@ -132,41 +132,21 @@ $(function() {
     duration: '401%',
     tweenChanges: true
   }).setPin('#car-screen').addTo(controller);
-  caption1_width = document.getElementById('car_caption1').offsetWidth;
-  caption1_width = caption1_width / document.documentElement.clientWidth * 100 + 100;
-  caption1_w = caption1_width + '%';
-  caption2_width = document.getElementById('car_caption2').offsetWidth;
-  caption2_width = caption2_width / document.documentElement.clientWidth * 100 + 100;
-  caption2_w = caption2_width + '%';
-  caption3_width = document.getElementById('car_caption3').offsetWidth;
-  caption3_width = caption3_width / document.documentElement.clientWidth * 100 + 100;
-  caption3_w = caption3_width + '%';
-  caption4_width = document.getElementById('car_caption4').offsetWidth;
-  caption4_width = caption4_width / document.documentElement.clientWidth * 100 + 100;
-  caption4_w = caption4_width + '%';
-  caption = new TimelineMax().to($('.section.main_car_animation .caption1'), 1, {
-    'left': caption1_w,
+  caption = new TimelineMax().to($('#car-screen .caption1'), 1, {
+    'left': '100%',
+    'transform': 'translateX(100%)',
     ease: Power0.easeNone
-  }).to($('.section.main_car_animation .caption2'), 1, {
-    'left': caption2_w,
+  }).to($('#car-screen .caption2'), 1, {
+    'left': '100%',
+    'transform': 'translateX(100%)',
     ease: Power0.easeNone
-  }).to($('.section.main_car_animation .caption3'), 1, {
-    'left': caption3_w,
+  }).to($('#car-screen .caption3'), 1, {
+    'left': '100%',
+    'transform': 'translateX(100%)',
     ease: Power0.easeNone
-  }).to($('.section.main_car_animation .caption4'), 1, {
-    'left': caption4_w,
-    ease: Power0.easeNone
-  }).to($('.section.main_car_animation .caption1'), 0, {
-    'left': '0%',
-    ease: Power0.easeNone
-  }).to($('.section.main_car_animation .caption2'), 0, {
-    'left': '0%',
-    ease: Power0.easeNone
-  }).to($('.section.main_car_animation .caption3'), 0, {
-    'left': '0%',
-    ease: Power0.easeNone
-  }).to($('.section.main_car_animation .caption4'), 0, {
-    'left': '0%',
+  }).to($('#car-screen .caption4'), 1, {
+    'left': '100%',
+    'transform': 'translateX(100%)',
     ease: Power0.easeNone
   });
   scene = new ScrollMagic.Scene({
